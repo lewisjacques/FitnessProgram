a054ddbf2b7f0b3deae7cd47f05e54930111a4b1
Lewis J Waite <lewiswaite@live.com>
2023-06-06 23:20:05 +0100

    Minor changes

--- update_program.py
+++ update_program.py
@@ -2,34 +2,29 @@ from program import Program
 import argparse
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(
-    #     prog='Fitness Program Functionality',
-    #     description="Update the program with the latest comments"+\
-    #         "Takes a raw copied and pasted comment file from the Google"+\
-    #         "Sheets API and adds the data into a sheet to keep track of"+\
-    #         "logged comments. \n\nFuture functionality will allow the"+\
-    #         "comments to be extracted through the API directly, this"+\
-    #         "isn't currently possible"
-    # )
-    # parser.add_argument(
-    #     '--raw_comments',
-    #     help='Raw text file taken from the Google Sheets UI'
-    # )
-    # #! Currently an unavailable option through the API
-    # parser.add_argument(
-    #     '--api_comments', default=False,
-    #     help='If True, access the available comments through the API too'
-    # )
+    parser = argparse.ArgumentParser(
+        prog='Fitness Program Functionality',
+        description="Update the program with the latest comments"+\
+            "Takes a raw copied and pasted comment file from the Google"+\
+            "Sheets API and adds the data into a sheet to keep track of"+\
+            "logged comments. \n\nFuture functionality will allow the"+\
+            "comments to be extracted through the API directly, this"+\
+            "isn't currently possible"
+    )
+    parser.add_argument(
+        '--raw_comments',
+        help='Raw text file taken from the Google Sheets UI'
+    )
+    #! Currently an unavailable option through the API
+    parser.add_argument(
+        '--api_comments', default=False,
+        help='If True, access the available comments through the API too'
+    )
 
-    # args = parser.parse_args()
-    # assert isinstance(args.api_comments, bool)
-
-    # prog = Program(
-    #     comment_file_path=args.raw_comments,
-    #     api_extract=args.api_comments
-    # )
+    args = parser.parse_args()
+    assert isinstance(args.api_comments, bool)
 
     prog = Program(
-        comment_file_path="data/comments_230511.txt",
-        api_extract=False
+        comment_file_path=args.raw_comments,
+        api_extract=args.api_comments
     )
